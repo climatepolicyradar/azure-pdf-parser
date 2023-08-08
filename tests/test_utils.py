062a24584e5aa59734c8ce88d76114cdf01a78cc
Mark <mark@climatepolicyradar.org>
2023-08-08 13:38:43 +0100

    Adding tests for md5sum.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+from azure_pdf_parser.utils import calculate_md5_sum, is_valid_md5
+
+
 def test_propagate_with_correct_page():
     pass
 
@@ -10,5 +13,20 @@ def test_split_into_pages():
     pass
 
 
-def test_calculate_md5_sum():
-    pass
+def test_is_valid_md5() -> None:
+    """Test that the function returns True for valid MD5 sums."""
+    assert not is_valid_md5("Invalid MD5 sum")
+
+    assert is_valid_md5("d41d8cd98f00b204e9800998ecf8427e")
+
+
+def test_calculate_md5_sum(one_page_pdf_bytes: bytes) -> None:
+    """Test that the md5 sum is calculated correctly."""
+
+    md5_sum = calculate_md5_sum(one_page_pdf_bytes)
+    assert isinstance(md5_sum, str)
+    assert is_valid_md5(md5_sum)
+
+    md5_sum = calculate_md5_sum(b"Random bytes!")
+    assert isinstance(md5_sum, str)
+    assert is_valid_md5(md5_sum)
