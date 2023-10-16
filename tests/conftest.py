47554fe70256dc9c77b0778b0f705ef76b4660a3
Joel Wright <joel@partiallycomplete.com>
2023-10-16 15:00:43 +0100

    Upgrade data-acess dependency to v0.1.9 (#30)

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,7 @@ def backend_document_json() -> dict:
         "source": "CCLW",
         "import_id": "TESTCCLW.executive.4.4",
         "family_import_id": "TESTCCLW.family.4.0",
+        "family_slug": "slug_TESTCCLW.family.4.0",
         "category": "Law",
         "geography": "EUR",
         "languages": ["English"],
