36cb4d99f0acb9a20752a179fe1aa0700b705773
Harrison Pim <hello@harrisonpim.com>
2024-05-23 16:33:31 +0100

    parse docs for UNECE sprint and upload them to s3 (#61)
    Adds a script to download, parse, and store a set of documents for the
    UNECE RAG labelling sprint.
    
    The resulting parser output json files (and the original pdf documents)
    are stored in the `cpr-unece-sprint` s3 bucket in the `labs` workspace
    ([link to
    bucket](https://eu-west-1.console.aws.amazon.com/s3/buckets/cpr-unece-sprint?region=eu-west-1&bucketType=general&tab=objects)).
    
    Document names are an ugly hash at the moment ([see
    comment](https://github.com/climatepolicyradar/azure-pdf-parser/blob/6cd5233a20741fd67eded1b1f7785c39ef1ec52b/scripts/parse_docs_for_unece_sprint.py#L139)).
    If you need something more readable, let me know and I'll figure out a
    workaround.

---
+++ scripts/unece_sprint/check_parsed_docs.py
@@ -0,0 +1,30 @@
+"""Check whether the UNECE documents can be loaded using the CPR SDK"""
+
+import boto3
+from cpr_data_access.parser_models import BaseParserOutput
+from rich.console import Console
+
+console = Console()
+
+
+session = boto3.Session(profile_name="labs")
+s3_client = session.client("s3", region_name="eu-west-1")
+bucket_name = "cpr-unece-sprint"
+
+# Check whether the bucket exists
+s3_client.head_bucket(Bucket=bucket_name)
+console.print(f"🪣 The '{bucket_name}' bucket exists", style="green")
+
+# List the objects in the bucket and get the first key
+response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix="output")
+keys = [obj["Key"] for obj in response["Contents"]]
+key = keys[0]
+console.print(f"📂 The bucket contains {len(keys)} json objects", style="green")
+
+# Get a document from the bucket
+response = s3_client.get_object(Bucket=bucket_name, Key=key)
+console.print(f"📄 Got a document from the bucket: {key}", style="green")
+
+# Check if the document can be loaded using the CPR SDK
+document = BaseParserOutput.model_validate_json(response["Body"].read())
+console.print("🥳 Document was successfully loaded by the CPR SDK", style="green")
